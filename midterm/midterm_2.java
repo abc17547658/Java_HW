f7c64e22094519665620b19b4951b52e9cde1c78
abc17547658 <92910454+abc17547658@users.noreply.github.com>
2021-11-22 11:56:39 +0800

    Add files via upload

---
+++ midterm/midterm_2.java
@@ -0,0 +1,31 @@
+class Dates {			
+	String[] weeks = new String[7];
+	
+	public Dates() {				
+		weeks[0] = "Sun";
+		weeks[1] = "Mon";
+		weeks[2] = "Thu";
+		weeks[3] = "Wed";
+		weeks[4] = "Tus";
+		weeks[5] = "Fri";
+		weeks[6] = "Sat";
+	}
+	public String askDate(int day) {			
+		if(day >= 1 && day <= 7)
+			return weeks[day % 7];
+		return "Wrong day";
+	
+	
+}
+	}
+
+public class midterm_2 {
+	public static void main(String[] args) {
+		
+		Dates aDate = new Dates();
+		for(int i =1;i<=7;i++) {
+			System.out.println(aDate.askDate(i));
+		}
+	}
+
+}
