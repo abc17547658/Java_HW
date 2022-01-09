8a6f5205f806686d891fc0719da49653bf9137fc
abc17547658 <92910454+abc17547658@users.noreply.github.com>
2022-01-10 06:52:20 +0800

    Add files via upload

---
+++ HW/HW13/HW13_5.java
@@ -0,0 +1,22 @@
+package HW13;
+
+public class HW13_5 {
+	public static void main(String args[]){
+		
+	      try
+	      {
+	         int num=12;
+	         int den[]={12,0,3,0,0,4};
+	         for(int i=0;i<10;i++)
+	            System.out.println("ans="+num/den[i]);
+	      }
+	      catch(ArithmeticException e)
+	      {
+	         System.out.println("除數為 0");
+	      }
+	      catch(ArrayIndexOutOfBoundsException e)
+	      {
+	         System.out.println("陣列索引值超出了範圍");
+	      }
+	   }
+}
