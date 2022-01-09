b11113e5f70ffc4a7d948fec7a4fca47e742a7e3
abc17547658 <92910454+abc17547658@users.noreply.github.com>
2022-01-10 03:23:05 +0800

    Add files via upload

--- HW/HW9/HW9_1.java
+++ HW/HW9/HW9_1.java
@@ -1,5 +1,34 @@
-package HomeWork;
+package HW9;
+
+class CRectangle{
+	int width;
+	int height;
+	
+	public CRectangle(int w,int h) {
+		width=w;
+		height=h;
+		System.out.println("");
+	}
+	public CRectangle() {
+		this(10,8);
+		System.out.println("");
+	}
+	public void show() {
+		System.out.println("width="+width);
+		System.out.println("height="+height);
+		
+	}
+	
+}
+
+
 
 public class HW9_1 {
+	public static void main(String args[]) {
+		CRectangle rec1=new CRectangle(5,2);
+		rec1.show();
+		CRectangle rec2=new CRectangle();
+		rec2.show();
+	}
 
 }
