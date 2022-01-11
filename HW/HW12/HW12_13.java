f3478543484da930a0231afd33773faa8a2515e6
abc17547658 <92910454+abc17547658@users.noreply.github.com>
2022-01-12 00:22:33 +0800

    Add files via upload

---
+++ HW/HW12/HW12_13.java
@@ -0,0 +1,8 @@
+package HW12;
+
+public class HW12_13 {	
+	public static void main(String args[]) {
+		System.out.println("最接近5.38的整數: "+Math.round(5.38));
+	}
+
+}
---
+++ HW/HW12/HW12_16.java
@@ -0,0 +1,14 @@
+package HW12;
+
+public class HW12_16 {
+	
+	public static void main(String args[]) {
+		
+		String str="262904713";
+	    int num;
+	    num=Integer.parseInt(str);
+	    System.out.println(num+"*2="+num*2);
+	}
+
+
+}
