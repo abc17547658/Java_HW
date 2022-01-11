fbf935ec555e40bf2e254074d2884185672e6326
abc17547658 <92910454+abc17547658@users.noreply.github.com>
2022-01-12 00:26:06 +0800

    Add files via upload

---
+++ HW/HW14/HW14_7.java
@@ -0,0 +1,21 @@
+package HW14;
+import java.io.*;
+public class HW14_7 {
+
+	public static void main(String args[]) throws IOException
+	   {
+	      int count=0;
+	      String str;
+	      FileReader fr=new FileReader("D:\\java\\donkey.txt");
+	      BufferedReader bfr=new BufferedReader(fr);
+
+	      while((str=bfr.readLine())!=null)
+	      {
+	         if(count==1)
+	            bfr.skip(14);
+	         System.out.println(str);
+	         count++;
+	      }
+	      fr.close();
+	   }
+}
